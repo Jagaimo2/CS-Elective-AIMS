e4f155c6a1f828d7479e5ed0e207cbb7371a95ac
LordGeese <horusagust@gmail.com>
2021-11-08 10:32:00 +0800

    updating to branch. will commit to main after verification

--- CSElectiveApplication/src/app/app.module.ts
+++ CSElectiveApplication/src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './pages/home/home.component';
+import { ContentHeaderComponent } from './content-header/content-header.component';
+import { FooterComponent } from './footer/footer.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    ContentHeaderComponent,
+    FooterComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
